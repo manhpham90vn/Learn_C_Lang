5fbfd7ac445c1158ebde8d5b698eee2bf047b438
Manh Pham <manhpham90vn@icloud.com>
2020-09-10 16:36:26 +0700

    7 Basic operatiors

---
+++ 7_Basic_Operators.c
@@ -0,0 +1,34 @@
+// Arithmetic Operators
+// +	Adds two operands.
+// −	Subtracts second operand from the first.
+// *	Multiplies both operands.
+// /	Divides numerator by de-numerator.
+// %	Modulus Operator and remainder of after an integer division.
+// ++	Increment operator increases the integer value by one.
+// --	Decrement operator decreases the integer value by one.
+
+// Relational Operators
+// ==	Checks if the values of two operands are equal or not. If yes, then the condition becomes true.
+// !=	Checks if the values of two operands are equal or not. If the values are not equal, then the condition becomes true.
+// >	Checks if the value of left operand is greater than the value of right operand. If yes, then the condition becomes true.
+// <	Checks if the value of left operand is less than the value of right operand. If yes, then the condition becomes true.
+// >=	Checks if the value of left operand is greater than or equal to the value of right operand. If yes, then the condition becomes true.
+// <=	Checks if the value of left operand is less than or equal to the value of right operand. If yes, then the condition becomes true.
+
+// Logical Operators
+// &&	Called Logical AND operator. If both the operands are non-zero, then the condition becomes true.
+// ||	Called Logical OR Operator. If any of the two operands is non-zero, then the condition becomes true.
+// !	Called Logical NOT Operator. It is used to reverse the logical state of its operand. If a condition is true, then Logical NOT operator will make it false.
+
+// Assignment Operators
+// =	Simple assignment operator. Assigns values from right side operands to left side operand
+// +=	Add AND assignment operator. It adds the right operand to the left operand and assign the result to the left operand.
+// -=	Subtract AND assignment operator. It subtracts the right operand from the left operand and assigns the result to the left operand.
+// *=	Multiply AND assignment operator. It multiplies the right operand with the left operand and assigns the result to the left operand.
+// /=	Divide AND assignment operator. It divides the left operand with the right operand and assigns the result to the left operand.
+// %=	Modulus AND assignment operator. It takes modulus using two operands and assigns the result to the left operand.
+// <<=	Left shift AND assignment operator.
+// >>=	Right shift AND assignment operator.
+// &=	Bitwise AND assignment operator.
+// ^=	Bitwise exclusive OR and assignment operator.
+// |=	Bitwise inclusive OR and assignment operator.
