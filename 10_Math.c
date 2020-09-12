54f6b0aaa4839ff37fe5fca52d6cc84dfe184714
Manh Pham <manhpham90vn@icloud.com>
2020-09-12 22:00:45 +0700

    10 Math

---
+++ 10_Math.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double a = 3.14;
+
+    printf("%lf\n", ceil(a)); // làm tròn lên
+    printf("%lf\n", floor(a)); // làm tròn xuống
+    printf("%lf\n", log10(10000));
+    printf("%d\n", (int)log10(10000)); // ép kiểu
+}
